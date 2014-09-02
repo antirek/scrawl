2ae6d661e9b5e4f11f5598e9c55c0fb779782ed3
sergey <serge.dmitriev@gmail.com>
2014-09-02 23:47:51 +0800

    some changes

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ var app = module.exports = express.createServer();
 
 app.configure(function(){
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
+  app.set('view engine', 'jade');  
   app.use(app.router);
   app.use(express.static(__dirname + '/public'));
 });
